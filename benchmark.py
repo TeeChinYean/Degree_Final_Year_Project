8413fe4f646d471338ece52f2b69ce9414dc44ba
TEE CHIN YEAN <128593175+TeeChinYean@users.noreply.github.com>
2026-01-24 01:43:51 +0800

    Update and rename benchmark.py to yolo/benchmark.py

--- benchmark.py
+++
@@ -1,4 +0,0 @@
-from ultralytics.utils.benchmarks import benchmark
-
-# Benchmark on GPU
-benchmark(model="F:/Degree_Final_Year_Project/runs/detect/fine2/train2/weights/best.pt", data="F:/Degree_Final_Year_Project/recycle/data.yaml", imgsz=640, half=False, device=0)
